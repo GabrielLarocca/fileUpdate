25478fdf7c935d7d3a60350b1881e055abf1f0ca
Gabriel Larocca <glarocca@laon.com.br>
2024-06-08 17:57:13 -0300

    🐛 fixes cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,25 @@ import { uploadFile, getUsers } from './controllers/userController';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
+const cors = require('cors');
+
+const allowedOrigins = [
+	'https://fileupdate-frontend-p03ni1tu7-gabriellaroccas-projects.vercel.app/',
+	'http://localhost:4000',
+];
+
+const corsOptions = {
+	origin: (origin: any, callback: any) => {
+		if (allowedOrigins.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+};
+
+app.use(cors(corsOptions));
+
 const uploadsDir = path.join(__dirname, 'uploads');
 const upload = multer({ dest: uploadsDir });
 
